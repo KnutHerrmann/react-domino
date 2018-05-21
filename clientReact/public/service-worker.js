ccc837fd627c65e8e2657e3f928e75979e4c078b
Knut Herrmann <knut.herrmann@leonso.de>
2018-05-21 07:58:54 +0200

    Data Caching with workbox' networkFirst strategy

--- clientReact/public/service-worker.js
+++ clientReact/public/service-worker.js
@@ -1,7 +1,15 @@
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js');
 
 workbox.precaching.precacheAndRoute([]);
+
+workbox.routing.registerRoute(
+    /(.*)data\.xsp\/data\/(.*)/,
+    workbox.strategies.networkFirst({
+        cacheName: 'theater-cache'
+    })
+);
 
 self.addEventListener('fetch', (event) => {
     console.log('serviceWorker fetch', event);
 });
+
--- serverNsf/WebContent/service-worker.js
+++ serverNsf/WebContent/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js');
 
 workbox.precaching.precacheAndRoute([
   {
@@ -31,6 +31,11 @@ workbox.precaching.precacheAndRoute([
   }
 ]);
 
+workbox.routing.registerRoute(/(.*)data\.xsp\/data\/(.*)/,
+    workbox.strategies.networkFirst({cacheName: 'theater-cache'})
+);
+
 self.addEventListener('fetch', (event) => {
     console.log('serviceWorker fetch', event);
 });
+
